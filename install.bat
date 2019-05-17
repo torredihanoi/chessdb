2b833fc44efa46e6a4a413ced074bf3910980af8
Benedetto Cattarinich <torredihanoi@gmail.com>
2019-05-17 15:28:27 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,10 @@
+REM call npx create-react-app example-create-react-app-express
+REM pause
+
+REM REM REM Create a /client directory under example-create-react-app-express directory and move all the React boilerplate code created by create-react-app to this new client directory.
+
+REM cd example-create-react-app-express
+REM call npm i nodemon -g
+REM pause
+REM call yarn
+REM pause
